aedf4619415d2724c0c7a30e2b2628dde2203e8a
Thiago <thiago.g.moro@hotmail.com>
2019-11-06 11:49:12 -0300

    Second commit

--- Desafio 03/desafio.js
+++ Desafio 03/desafio.js
@@ -1,6 +1,6 @@
-let sol = ['a', 'm', 'b', 'i', 't', 'u', 's', '.', 'i', 'o']
-let res = document.querySelector('div#res')
-let n = ' '
+let sol = ['a', 'm', 'b', 'i', 't', 'u', 's', '.', 'i', 'o'];
+let res = document.querySelector('div#res');
+let n = ' ';
 
 function solSettime() {
   for (let i = 0; i < sol.length; i++) {
