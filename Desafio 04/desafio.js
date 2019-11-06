1b3419369048d0ce2b21551e8c477a1eb2b2fbdf
Thiago <thiago.g.moro@hotmail.com>
2019-11-06 11:49:55 -0300

    Second commit

--- Desafio 04/desafio.js
+++ Desafio 04/desafio.js
@@ -1,11 +1,15 @@
-var pessoa = {
-  nome: ['Thiago', 'Caio', 'Bruno'],
-  idade: [4567, 8, 24]
-}
+const pessoa = [
+  { name: 'Thiago', idade: 4567 },
+  { name: 'Caio', idade: 8 },
+  { name: 'Bruno', idade: 24 }
+];
 var res = document.querySelector('div#res')
 
 function somentenome() {
-  for (var n = 0; n < pessoa.nome.length; n++) {
-    res.innerHTML += `${pessoa.nome[n]}<br>`
+  for (let i = 0; i < pessoa.length; i++) {
+    var byName = function (object) {
+      return object.name
+    };
+    res.innerHTML += `${byName(pessoa[i])} <br/>`
   }
 }
