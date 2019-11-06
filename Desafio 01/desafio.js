355d18ce91f7c192db7f5098dc7d190465be7054
Thiago <thiago.g.moro@hotmail.com>
2019-11-06 11:46:30 -0300

    Second commit

--- Desafio 01/desafio.js
+++ Desafio 01/desafio.js
@@ -1,5 +1,5 @@
-const sol = ['a', 'm', 'b', 'i', 't', 'u', 's', '.', 'i', 'o']
-const res = document.querySelector('div#res')
+const sol = ['a', 'm', 'b', 'i', 't', 'u', 's', '.', 'i', 'o'];
+const res = document.querySelector('div#res');
 
 function soletrando() {
   for (let n = 0; n < sol.length; n++) {
